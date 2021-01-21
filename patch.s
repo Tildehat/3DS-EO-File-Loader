57d40b875a1008819c29cbf09c9727868165afe6
Tildehat <timepen1@gmail.com>
2021-01-21 10:48:14 -0600

    Add rom file check to when verifying if SE or Batttle models are loaded. Useful for adding battle models or SE not present in the HPB

--- patch.s
+++ patch.s
@@ -13,4 +13,17 @@ checkFileExist:
 .org 0x11911c
 	b checkFileExist
 
+;when grabbing battle models and sounds does another check for the file? Possibly for DLC. Might break EOV/EO2U functionality if ported
+.org 0x310668 ;when files get loaded
+	b 0x31068c ; skip the check if HPB is loaded
+checkFileExistModelAndSound:
+	tst r0, #0x80000000
+	bne 0x31067c	;no file in the rom check HPB
+	mov	r5,r0
+	b	0x31074c	;found file load from rom path
+	
+.org 0x310748
+	b checkFileExistModelAndSound
+
+
 .Close
